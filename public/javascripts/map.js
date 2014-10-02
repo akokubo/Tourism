df7b768601d391fb9542bc6c350f6b6f1d41d396
sugo-shota <si12015@edu.aomori-u.ac.jp>
2014-10-02 12:44:28 +0900

    map.jsとmapのviewの編集

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,7 +1,4 @@
-
-
-var MAPRAMBLE = {};
-
+MAPRAMBLE.markers = [];
 
 MAPRAMBLE.addPlaceMarkers = function () {
     'use strict';
@@ -27,27 +24,6 @@ MAPRAMBLE.addMarker = function (place) {
     // マーカーを配列に入れる
     this.markers.push(marker);
 };
-
-
-
-MAPRAMBLE.markers = [];
-
-MAPRAMBLE.places = [];
-
-MAPRAMBLE.places.push({lat:40.82706,lng:140.735793,id:1});//アウガ
-MAPRAMBLE.places.push({lat:40.8269467,lng:140.7372391,id:2});//パサージュ広場
-MAPRAMBLE.places.push({lat:40.829591,lng:140.741032,id:3});//アスパム
-MAPRAMBLE.places.push({lat:40.8245008,lng:140.7364937,id:4});//ワラッセ
-MAPRAMBLE.places.push({lat:40.8245008,lng:140.7364937,id:5});//a-factory
-//MAPRAMBLE.places.push({lat:40.1,lng:140.5,id:1});
-//MAPRAMBLE.places.push({lat:40.1,lng:140.5,id:1});
-//MAPRAMBLE.places.push({lat:40.1,lng:140.5,id:1});
-//MAPRAMBLE.places.push({lat:40.1,lng:140.5,id:1});
-//MAPRAMBLE.places.push({lat:40.1,lng:140.5,id:1});
-//MAPRAMBLE.places.push({lat:40.1,lng:140.5,id:1});
-//MAPRAMBLE.places.push({lat:40.1,lng:140.5,id:1});
-
-
 
 MAPRAMBLE.createMap = function (initial) {
     'use strict';
@@ -83,56 +59,14 @@ MAPRAMBLE.setEventHandler = function () {
         MAPRAMBLE.setHeight();
     });
 
-    if (this.mode === 'edit') {
-        google.maps.event.addListener(this.map, 'click', function (event) {
-            $.mobile.changePage('/places/new?lat=' + event.latLng.lat() + '&lng=' + event.latLng.lng());
-        });        
-    }
 };
-
-/*
-MAPRAMBLE.fitBounds = function () {
-    'use strict';
-    this.map.fitBounds(this.bounds);
-};
-*/
-
-MAPRAMBLE.poryline = function () {
-    var route = [
-        new google.maps.LatLng(40.828713,140.734682),
-        new google.maps.LatLng(40.828470, 140.735610),
-        new google.maps.LatLng(40.828177, 140.735456),
-        new google.maps.LatLng(40.827718, 140.736536),
-        new google.maps.LatLng(40.827552, 140.736434),
-        new google.maps.LatLng(40.827170, 140.737325),
-        new google.maps.LatLng(40.8273607,140.7371667),
-        new google.maps.LatLng(40.827121, 140.737446),
-        new google.maps.LatLng(40.826815, 140.738243),
-        new google.maps.LatLng(40.828134, 140.738749),
-        new google.maps.LatLng(40.829587, 140.739500),
-        new google.maps.LatLng(40.828832,140.7388889),//アスパム
-        //new google.maps.LatLng(),
-        //new google.maps.LatLng(),
-    ];
-    var polyOptions = {
-    path: route,
-    strokeColor: "#0000ff",
-    strokeOpacity: 0.5,
-    strokeWeight: 5
-  }
-  var poly = new google.maps.Polyline(polyOptions);
-  return poly;
-};
-
 
 $(document).on('pageshow', function () {
 	'use strict';
     MAPRAMBLE.setHeight();
     MAPRAMBLE.map = MAPRAMBLE.createMap({zoom: 16, lat: 40.828713, lng: 140.734682});
     MAPRAMBLE.addPlaceMarkers();
-    //MAPRAMBLE.fitBounds();
     MAPRAMBLE.setEventHandler();
-    MAPRAMBLE.poryline().setMap(MAPRAMBLE.map);
 });
 
 
